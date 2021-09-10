b5dd2fe0662978f6d7481a63b728d261c1c3c79e
Mario González Rodríguez <marsgr6@gmail.com>
2021-09-09 21:05:12 -0500

    c3

---
+++ ICP2021/internals.pl
@@ -0,0 +1,10 @@
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# Associate internals original text with physical files.
+
+
+$key = q/ssp1/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
